18e9b37c76b7d3596515be243f65e67358b978ba
Leo Tanaka <79670101+AnotherLyat@users.noreply.github.com>
2022-04-15 15:04:41 -0300

    Update Ex2.java

--- Ex2.java
+++ Ex2.java
@@ -20,23 +20,27 @@ public class Ex2{
             
             System.out.print(" Digite o numero da carteira, digite 0 para sair: ");
             card = imput.nextInt();
-            
-            do{
-                number= number+1;
-                System.out.print("\n Digite o valor da multa: \n");
-                valor = imput.nextDouble();
-                valortotal= valortotal+valor;
+           
+            if(card!=0){
+                do{
+                    number= number+1;
+                    System.out.print("\n Digite o valor da multa: \n");
+                    valor = imput.nextDouble();
+                    valortotal= valortotal+valor;
 
-                System.out.print("\nDigite 'sim' mudar a carteira sendo multada: \n");
-                imput.nextLine();
-                x = imput.nextLine();
+                    System.out.print("\nDigite 'sim' mudar a carteira sendo multada: \n");
+                    imput.nextLine();
+                    x = imput.nextLine();
                 
-                if(x.equals("sim")){
-                    mvalor.add(number);
-                    System.out.print("\nO dono da carteira "+ card+" recebeu " +number+ " multas, com um total de R$"+ valortotal+ "\n\n");
-                }
+                    if(x.equals("sim")){
+                        mvalor.add(number);
+                        System.out.print("\nO dono da carteira "+ card+" recebeu " +number+ " multas, com um total de R$"+ valortotal+ "\n\n");
+                    }
 
-            }while(!x.equals("sim"));    
+                }while(!x.equals("sim")); 
+            }//programa trava quando digito "0" para o valor de card, não sei se o error está na minha maquina, funciona normal se remover o if, porem a ausencia do if causaria leitura uma desnecessária dentro do do{}while 
+
+          
             number=0;
             valor=0;
             mcard.add(card);
