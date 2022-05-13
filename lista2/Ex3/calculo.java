d0b8bf10f1874dd89d704f30ac066540b433a308
Leo Tanaka <79670101+AnotherLyat@users.noreply.github.com>
2022-05-13 19:22:22 -0300

    Update calculo.java

--- lista2/Ex3/calculo.java
+++ lista2/Ex3/calculo.java
@@ -5,9 +5,17 @@ public class calculo {
         res = Math.pow(a,b);
         return res;
     }
+    public double pot(String a, String b){
+        res = Math.pow(Double.parseDouble(a), Double.parseDouble(b));
+        return res;
+    }
 
     public double rad(Double a, Double b){
         res = Math.pow(a, 1/b);
+        return res;
+    }
+    public double rad(String a, String b){
+        res = Math.pow(Double.parseDouble(a), 1/Double.parseDouble(b));
         return res;
     }
     
